d7dcb8c1bd16a51bbe1b539985f9f25a747a6a1c
Zaki Alam <48445346+zalam003@users.noreply.github.com>
2023-11-14 12:22:35 -0500

    add energi chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+// set variables from .env file
+const { RPC_URL, PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
@@ -15,7 +18,45 @@ module.exports = {
   networks: {
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API}`,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${PRIVATE_KEY}`],
     },
+    energiMainnet: {
+      chainId: 39797,
+      url: String(RPC_URL || "https://nodeapi.energi.network"),
+      gas: 1000000,
+      gasPrice: 20000000000, // 20 GWei
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    energiTestnet: {
+      chainId: 49797,
+      url: String(RPC_URL || "https://nodeapi.test.energi.network"),
+      gas: 1000000,
+      gasPrice: 20000000000, // 20 GWei
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      energiTestnet: 'xxxxx-no-api-key-needed-xxxxx',
+      energiMainnet: 'xxxxx-no-api-key-needed-xxxxx'
+    },
+    customChains: [
+      {
+        network: "energiMainnet",
+        chainId: 39797,
+        urls: {
+          apiURL: "https://explorer.energi.network/api",
+          browserURL: "https://explorer.energi.network"
+        },
+      },
+      {
+        network: "energiTestnet",
+        chainId: 49797,
+        urls: {
+          apiURL: "https://explorer.test.energi.network/api",
+          browserURL: "https://explorer.test.energi.network"
+        },
+      },
+    ]
   },
 };
